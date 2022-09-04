6feb084788c06e599f92d2f6a65f64858ad29ae9
Anattagarbha <ruslanvic@gmail.com>
2022-09-04 17:27:34 +0300

    comment added

--- 074-chaining-if-else-statements.js
+++ 074-chaining-if-else-statements.js
@@ -1,20 +1,38 @@
+/*
+
+Write chained if/else if statements to fulfill the following conditions:
+
+num < 5 - return Tiny
+num < 10 - return Small
+num < 15 - return Medium
+num < 20 - return Large
+num >= 20 - return Huge
+
 function testSize(num) {
   // Only change code below this line
-if (num < 5) {
-  return "Tiny";
+
+
+  return "Change Me";
+  // Only change code above this line
 }
-else if (num < 10) {
-  return "Small";
-}
-else if (num < 15) {
-  return "Medium";
-}
-else if (num < 20) {
-  return "Large";
-}
-else {
-  return "Huge";
-}
+
+testSize(7);
+
+*/
+
+function testSize(num) {
+  // Only change code below this line
+  if (num < 5) {
+    return "Tiny";
+  } else if (num < 10) {
+    return "Small";
+  } else if (num < 15) {
+    return "Medium";
+  } else if (num < 20) {
+    return "Large";
+  } else {
+    return "Huge";
+  }
   // Only change code above this line
 }
 
