fe0100f084d86a21c0d481ba983820d7f86381a7
Anattagarbha <ruslanvic@gmail.com>
2022-09-04 17:06:40 +0300

    comment added

--- 071-introducing-else-statements.js
+++ 071-introducing-else-statements.js
@@ -1,3 +1,7 @@
+/*
+
+Combine the if statements into a single if/else statement.
+
 function testElse(val) {
   let result = "";
   // Only change code below this line
@@ -6,7 +10,25 @@ function testElse(val) {
     result = "Bigger than 5";
   }
 
-  else {
+  if (val <= 5) {
+    result = "5 or Smaller";
+  }
+
+  // Only change code above this line
+  return result;
+}
+
+testElse(4);
+
+*/
+
+function testElse(val) {
+  let result = "";
+  // Only change code below this line
+
+  if (val > 5) {
+    result = "Bigger than 5";
+  } else {
     result = "5 or Smaller";
   }
 
