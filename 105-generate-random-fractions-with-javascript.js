306aa433725e4463b9c1c703af69a4230f8dd791
Anattagarbha <ruslanvic@gmail.com>
2022-09-06 13:52:58 +0300

    comment added

--- 105-generate-random-fractions-with-javascript.js
+++ 105-generate-random-fractions-with-javascript.js
@@ -1,5 +1,19 @@
+/*
+
+Change randomFraction to return a random number instead of returning 0.
+
 function randomFraction() {
 
+  // Only change code below this line
+
+  return 0;
+
+  // Only change code above this line
+}
+
+*/
+
+function randomFraction() {
   // Only change code below this line
 
   return Math.random();
