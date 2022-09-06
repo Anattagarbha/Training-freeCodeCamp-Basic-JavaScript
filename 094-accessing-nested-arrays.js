f8d06e44700da2b102f910e0fab62796fab620af
Anattagarbha <ruslanvic@gmail.com>
2022-09-06 13:05:51 +0300

    comment added

--- 094-accessing-nested-arrays.js
+++ 094-accessing-nested-arrays.js
@@ -1,3 +1,8 @@
+/*
+
+Using dot and bracket notation, set the variable secondTree 
+to the second item in the trees list from the myPlants object.
+
 const myPlants = [
   {
     type: "flowers",
@@ -15,6 +20,21 @@ const myPlants = [
       "birch"
     ]
   }
+];
+
+const secondTree = "";
+
+*/
+
+const myPlants = [
+  {
+    type: "flowers",
+    list: ["rose", "tulip", "dandelion"],
+  },
+  {
+    type: "trees",
+    list: ["fir", "pine", "birch"],
+  },
 ];
 
 const secondTree = myPlants[1].list[1];
