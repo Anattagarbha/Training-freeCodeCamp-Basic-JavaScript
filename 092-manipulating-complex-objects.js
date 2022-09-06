18b1c855375d35db15caff702c27f755b8c983c4
Anattagarbha <ruslanvic@gmail.com>
2022-09-06 12:59:31 +0300

    comment added

--- 092-manipulating-complex-objects.js
+++ 092-manipulating-complex-objects.js
@@ -1,3 +1,8 @@
+/*
+
+Add a new album to the myMusic array. Add artist and title strings, 
+release_year number, and a formats array of strings.
+
 const myMusic = [
   {
     "artist": "Billy Joel",
@@ -9,9 +14,23 @@ const myMusic = [
       "LP"
     ],
     "gold": true
+  }
+];
+
+*/
+
+const myMusic = [
+  {
+    artist: "Billy Joel",
+    title: "Piano Man",
+    release_year: 1973,
+    formats: ["CD", "8T", "LP"],
+    gold: true,
   },
-  {"artist": "The Doors",
-  "title": "Strange Days",
-  "release_year": 1967,
-  "formats": ["CD", "Cassette", "LP"]}
+  {
+    artist: "The Doors",
+    title: "Strange Days",
+    release_year: 1967,
+    formats: ["CD", "Cassette", "LP"],
+  },
 ];
