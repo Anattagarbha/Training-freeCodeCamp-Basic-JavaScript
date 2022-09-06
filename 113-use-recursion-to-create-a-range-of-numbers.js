8f34ccc8636e16bf23ca13bcd75f6f0c04417be3
Anattagarbha <ruslanvic@gmail.com>
2022-09-06 14:13:46 +0300

    comment added

--- 113-use-recursion-to-create-a-range-of-numbers.js
+++ 113-use-recursion-to-create-a-range-of-numbers.js
@@ -1,3 +1,19 @@
+/*
+
+We have defined a function named rangeOfNumbers with two parameters. 
+The function should return an array of integers which begins with a number 
+represented by the startNum parameter and ends with a number represented 
+by the endNum parameter. The starting number will always be less than 
+or equal to the ending number. Your function must use recursion by 
+calling itself and not use loops of any kind. It should also work for 
+cases where both startNum and endNum are the same.
+
+function rangeOfNumbers(startNum, endNum) {
+  return [];
+};
+
+*/
+
 function rangeOfNumbers(startNum, endNum) {
   if (endNum < startNum) {
     return [];
@@ -6,4 +22,4 @@ function rangeOfNumbers(startNum, endNum) {
     numbers.push(endNum);
     return numbers;
   }
-};
+}
